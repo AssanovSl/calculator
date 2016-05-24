91a7bdcae29746809f4628487c995b0bf253d378
caIIIka17 <17timcarter17@gmail.com>
2016-05-25 00:58:28 +0600

    2 test one argument

---
+++ CalculatorTest/OneArgTests/RootTests.cs
@@ -0,0 +1,20 @@
+﻿using Calculator;
+using NUnit.Framework;
+
+
+namespace CalculatorTest.OneArgTests
+{
+    [TestFixture]
+    public class RootTests
+    {
+        [TestCase(9, 3)]
+        [TestCase(25, 5)]
+        [TestCase(225, 15)]
+        public void SqrtTest(double firstValue, double expected)
+        {
+            IOneCalculator calculator = new Root();
+            double result = calculator.OneArgCalculate(firstValue);
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
---
+++ CalculatorTest/OneArgTests/SinusTests.cs
@@ -0,0 +1,19 @@
+﻿using Calculator;
+using NUnit.Framework;
+
+namespace CalculatorTest.OneArgTests
+{
+    [TestFixture]
+    public class SinusTests
+    {
+        [TestCase(0, 0)]
+        [TestCase(1, 0.84147)]
+        [TestCase(4, -0.75680)]
+        public void SinTest(double firstValue, double expected)
+        {
+            IOneCalculator calculator = new Sinus();
+            double result = calculator.OneArgCalculate(firstValue);
+            Assert.AreEqual(expected, result, 0.00001);
+        }
+    }
+}
